2b0f4974151f8c3f41063dec416d706f252753b2
Romain-14 <romain14fmsn.com>
2023-04-11 13:30:27 +0200

    updta key and value image

--- data/index.js
+++ data/index.js
@@ -3,7 +3,7 @@ const datas = {
         title: ["Today's ", "Special"],
         crustyFish : {
             title: "The Crusty fish",
-            img: {url : "./img/recipe1.jpg", alt: "fresh fries fish with some green pees and a white sauce"},
+            img: {src : "recipe1.jpg", alt: "fresh fries fish with some green pees and a white sauce"},
             para: [
                 {p1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et? rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?",},
                 {p2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?",},
@@ -13,7 +13,7 @@ const datas = {
         },
         multiFish : {
             title: "The Multi fish",
-            img: {url : "./img/recipe2.jpg", alt: "crevettes, coquillages in different white cup and a green sauce"},
+            img: {src : "recipe2.jpg", alt: "crevettes, coquillages in different white cup and a green sauce"},
             para: [
                 {p1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et? rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?",},
                 {p2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?rem dicta incidunt autem nulla amet laboriosam quos nostrum eveniet et?",},
